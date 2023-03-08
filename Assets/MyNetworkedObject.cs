3d92074314e02e915c9611dad5b1ca0166dfe10b
andreibunea99 <56512139+andreibunea99@users.noreply.github.com>
2023-03-08 21:17:14 +0000

    Network script

--- Assets/MyNetworkedObject.cs
+++ Assets/MyNetworkedObject.cs
@@ -5,15 +5,24 @@ using Ubiq.Messaging;
 using Ubiq.Spawning;
 using Ubiq.XR;
 
-public class MyNetworkedObject : MonoBehaviour, INetworkSpawnable, IGraspable
+public class MyNetworkedObject : MonoBehaviour, IGraspable
 {
 
     // Spawn with peer scope ???
     public NetworkId NetworkId { get; set; }
-
+    private bool owner;
     private Hand controller;
-
+    Vector3 lastPosition;
     NetworkContext context;
+
+    private struct Message
+    {
+        public Vector3 position;
+
+        public Message(Transform transform) {
+            this.position = transform.position;
+        }
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -21,47 +30,41 @@ public class MyNetworkedObject : MonoBehaviour, INetworkSpawnable, IGraspable
         context = NetworkScene.Register(this);
     }
 
-    Vector3 lastPosition;
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (lastPosition != transform.localPosition)
-        {
-            lastPosition = transform.localPosition;
-            context.SendJson(new Message { position = transform.localPosition });
-        }
-    }
-
-    private struct Message
-    {
-        public Vector3 position;
-    }
-
     public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
     {
-        var m = message.FromJson<Message>();
+        var data = message.FromJson<Message>();
 
-        transform.localPosition = m.position;
+        transform.position = data.position;
 
-        lastPosition = transform.localPosition;
+        lastPosition = transform.position;
+    }
+
+    private void FixedUpdate()
+    {
+        if (owner)
+        {
+            // 4. Send transform update messages if we are the current 'owner'
+            context.SendJson(new Message(transform));
+        }
     }
 
     private void LateUpdate() {
         if (controller)
         {
             transform.position = controller.transform.position;
-            transform.rotation = controller.transform.rotation;
+            // transform.rotation = controller.transform.rotation;
         }
     }
 
     void IGraspable.Grasp(Hand controller)
     {
+        owner = true;
         this.controller = controller;
     }
 
     void IGraspable.Release(Hand controller)
     {
+        owner = false;
         this.controller = null;
     }
 }
