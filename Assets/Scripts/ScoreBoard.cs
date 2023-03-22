1ef319d037bddaf94434d27dbd24331506e214fc
andreibunea99 <56512139+andreibunea99@users.noreply.github.com>
2023-03-22 22:54:15 +0000

    Score network

--- Assets/Scripts/ScoreBoard.cs
+++ Assets/Scripts/ScoreBoard.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
 using UnityEngine.UI;
-using Ubiq.NetworkedBehaviour;
+using Ubiq.Messaging;
 
-public class ScoreBoard: MonoBehaviour
+public class ScoreBoard : MonoBehaviour
 {
     public Text puttsCounter;
+
     private int putts;
+
+    NetworkContext context;
+
+    private void Start()
+    {
+        context = NetworkScene.Register(this);
+    }
+
+    private struct Message
+    {
+        public int putts;
+    }
+
+    public void ProcessMessage(ReferenceCountedSceneGraphMessage message)
+    {
+        // Parse the message
+        var m = message.FromJson<Message>();
+
+        putts = m.putts;
+        puttsCounter.text = putts.ToString();
+    }
 
     public void AddPutt()
     {
         putts++;
         puttsCounter.text = putts.ToString();
+
+        context.SendJson(new Message()
+        {
+            putts = putts
+        });
     }
 
     public void ResetPutts()
