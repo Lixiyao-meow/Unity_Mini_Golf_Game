9ecd4c8024608eb30ce3d7683068b228e7a70586
andreibunea99 <56512139+andreibunea99@users.noreply.github.com>
2023-03-22 13:21:21 +0000

    patch spawner

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -17,6 +17,26 @@ public class Spawner : MonoBehaviour
     public GameObject turnip;
     public GameObject stone;
 
+    public GameObject patch0;
+    public GameObject patch1;
+    public GameObject patch2;
+    public GameObject patch3;
+    public GameObject patch4;
+    public GameObject patch5;
+    public GameObject patch6;
+    public GameObject patch7;
+    public GameObject patch8;
+    public GameObject patch9;
+    public GameObject patch10;
+    public GameObject patch11;
+    public GameObject patch12;
+    public GameObject patch13;
+    public GameObject patch14;
+    public GameObject patch15;
+    public GameObject patch16;
+    public GameObject patch17;
+    public GameObject patch18;
+
 
     NetworkContext context;
 
@@ -43,6 +63,25 @@ public class Spawner : MonoBehaviour
         if (id == 7) return carrot;
         if (id == 8) return turnip;
         if (id == 9) return stone;
+        if (id == 10) return patch0;
+        if (id == 11) return patch1;
+        if (id == 12) return patch2;
+        if (id == 13) return patch3;
+        if (id == 14) return patch4;
+        if (id == 15) return patch5;
+        if (id == 16) return patch6;
+        if (id == 17) return patch7;
+        if (id == 18) return patch8;
+        if (id == 19) return patch9;
+        if (id == 20) return patch10;
+        if (id == 21) return patch11;
+        if (id == 22) return patch12;
+        if (id == 23) return patch13;
+        if (id == 24) return patch14;
+        if (id == 25) return patch15;
+        if (id == 26) return patch16;
+        if (id == 27) return patch17;
+        if (id == 28) return patch18;
 
         return null;
     }
